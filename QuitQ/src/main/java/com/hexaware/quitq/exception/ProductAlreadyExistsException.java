4f0fca8b62b3b17d41715f5a2a4db8abc6147afe
yash shrivastava <yashshrivastava25@gmail.com>
2024-11-21 16:47:54 +0530

    Deleted conflicting folders from master branch

--- QuitQ/src/main/java/com/hexaware/quitq/exception/ProductAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.quitq.exception;
-
-public class ProductAlreadyExistsException extends RuntimeException{
-
-	public ProductAlreadyExistsException(String message) {
-		super(message);
-	}
-
-}
